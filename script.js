d45d89c73f413ffae1fb6ba298e18841ef878552
RiskierGriffin <tenenbaumallan@protonmail.com>
2019-07-22 18:21:43 -0400

    added .txt export

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ function makeid(length) {
     }
     return result;
 }
+
+function exportEmails(){
+    var text = document.getElementById("emails").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "emails.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
